554becf15589ce5e456aa800898522b7c2dca73c
jampus90 <alexandre.yama2@gmail.com>
2026-02-23 22:03:46 -0300

    Feat: initial UI setup with bootstrap

--- sistemadevenda/vendas/views.py
+++ sistemadevenda/vendas/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the vendas index.")
+    return render(request, 'base.html')
 
+def login(request):
+    return render(request, 'login.html')
 # Create your views here.
