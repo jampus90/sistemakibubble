bdb4de8d2347d102ae2e8faaa944a6a8035f33d6
jampus90 <alexandre.yama2@gmail.com>
2026-02-23 22:51:25 -0300

    feat: Login almost functional

--- sistemadevenda/vendas/views.py
+++ sistemadevenda/vendas/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
 
 def index(request):
     return render(request, 'base.html')
 
 def login(request):
-    return render(request, 'login.html')
-# Create your views here.
+    user_list = User.objects.all()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password', 'user_list': user_list})
+    return render(request, 'login.html', {'user_list': user_list})
+
