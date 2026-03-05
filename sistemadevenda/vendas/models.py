a2753436340074ad766e03a52a131ada26d793bf
jampus90 <alexandre.yama2@gmail.com>
2026-03-04 23:24:02 -0300

    Feat: Tables creations

--- sistemadevenda/vendas/models.py
+++ sistemadevenda/vendas/models.py
@@ -1,4 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    numero_wpp = models.CharField(max_length=20, blank=True, null=True, verbose_name="WhatsApp")
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade_estoque = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nome
+
+class Venda(models.Model):
+    funcionario = models.ForeignKey(User, on_delete=models.PROTECT, related_name='vendas')
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True, blank=True, related_name='compras')  
+    data_hora = models.DateTimeField(auto_now_add=True)
+    total_venda = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"Venda #{self.id} - {self.data_hora.strftime('%d/%m/%Y %H:%M')}"
+
+class ItemVenda(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name='itens')  
+    produto = models.ForeignKey(Produto, on_delete=models.PROTECT)  
+    quantidade = models.PositiveIntegerField()
+    preco_na_hora = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantidade}x {self.produto.nome} (Venda #{self.venda.id})"
