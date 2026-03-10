ca59d0a6b4b386b7353c9edc2949d4437ecea32b
jampus90 <alexandre.yama2@gmail.com>
2026-03-09 23:44:13 -0300

    Fix: Mudança no nome de variáveis para prod

--- sistemadevenda/mysite/settings.py
+++ sistemadevenda/mysite/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ['DB_NAME'],
-        "USER": os.environ['DB_USER'],
-        "PASSWORD": os.environ['DB_PASSWORD'],
-        "HOST": os.getenv('DB_HOST', 'localhost'),
-        "PORT": os.getenv('DB_PORT', '5432'),
+        "NAME": os.environ['POSTGRES_DB'],
+        "USER": os.environ['POSTGRES_USER'],
+        "PASSWORD": os.environ['PGPASSWORD'],
+        "HOST": os.getenv('DATABASE_URL', 'localhost'),
+        "PORT": os.getenv('PGPORT', '5432'),
     }
 }
 
